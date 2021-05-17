a2cf363664b4244db434cb184cba7937ca68a4ef
Marco Orazio Re <marcooraziore@gmail.com>
2021-05-17 18:13:46 +0200

    Current series section
    - Building current series section

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -2,6 +2,27 @@
 
 @section('content')
     <section class="jumbotron">
-        
+
+    </section>
+
+    <section class="current-series">
+        <div class="container">
+            <div class="current-series-title">
+                <span>CURRENT SERIES</span>
+            </div>
+            <div class="current-series-wrapper">
+                <ul>
+                    @foreach ($data as $item)
+                        <li>
+                            <img src="{{ $item['thumb'] }}" alt="">
+                            <span>{{ $item['title']}}</span>
+                        </li>
+                    @endforeach
+                </ul>
+            </div>
+            <div class="load-more">
+                <button class="load-more-btn">LOAD MORE</button>            
+            </div>
+        </div>
     </section>
 @endsection
