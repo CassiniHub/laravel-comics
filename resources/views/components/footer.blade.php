11653b3988c50642f254164cb383159a1e234a14
Marco Orazio Re <marcooraziore@gmail.com>
2021-05-17 22:14:19 +0200

    Footer section
    - Footer section completed
    
    Known Issues:
    - Needed detail review

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,1 +1,164 @@
-<h1>i am footer</h1>
+{{-- FOOTER --}}
+<footer>
+    <div class="primary-footer">
+        <div class="primary-footer-bg">
+            
+        </div>
+
+        <div class="container">
+            <div class="primary-footer-wrapper">
+
+                <div class="primary-footer-left">
+                    <div class="footer-links">
+                        <div class="footer-col">
+                            <h3>
+                                DC COMICS
+                            </h3>
+                            <ul>
+                                <li>
+                                    <a href="">Characters</a>
+                                </li>
+                                <li>
+                                    <a href="">Comics</a>
+                                </li>
+                                <li>
+                                    <a href="">Movies</a>
+                                </li>
+                                <li>
+                                    <a href="">TV</a>
+                                </li>
+                                <li>
+                                    <a href="">Games</a>
+                                </li>
+                                <li>
+                                    <a href="">Videos</a>
+                                </li>
+                                <li>
+                                    <a href="">News</a>
+                                </li>
+                            </ul>
+    
+                            <h3>
+                                SHOP
+                            </h3>
+                            <ul>
+                                <li>
+                                    <a href="">Shop DC</a>
+                                </li>
+                                <li>
+                                    <a href="">Shop DC Collectibles</a>
+                                </li>
+                            </ul>
+                        </div>
+                        <div class="footer-col">
+                            <h3>
+                                DC
+                            </h3>
+                            <ul>
+                                <li>
+                                    <a href="">Terme Of Use</a>
+                                </li>
+                                <li>
+                                    <a href="">Privacy Policy (New)</a>
+                                </li>
+                                <li>
+                                    <a href="">Ad Choiches</a>
+                                </li>
+                                <li>
+                                    <a href="">Advertising</a>
+                                </li>
+                                <li>
+                                    <a href="">Jobs</a>
+                                </li>
+                                <li>
+                                    <a href="">Subscriptions</a>
+                                </li>
+                                <li>
+                                    <a href="">Talent Workshop</a>
+                                </li>
+                                <li>
+                                    <a href="">CPSC Certificates</a>
+                                </li>
+                                <li>
+                                    <a href="">Ratings</a>
+                                </li>
+                                <li>
+                                    <a href="">Shop Help</a>
+                                </li>
+                                <li>
+                                    <a href="">Contact Us</a>
+                                </li>
+                            </ul>
+                        </div>
+                        <div class="footer-col">
+                            <h3>
+                                SITES
+                            </h3>
+                            <ul>
+                                <li>
+                                    <a href="">DC</a>
+                                </li>
+                                <li>
+                                    <a href="">MAD Magazines</a>
+                                </li>
+                                <li>
+                                    <a href="">DC Kids</a>
+                                </li>
+                                <li>
+                                    <a href="">DC Univers</a>
+                                </li>
+                                <li>
+                                    <a href="">DC Power Visa</a>
+                                </li>
+                            </ul>
+                        </div>
+    
+                    </div>
+
+                    <div class="footer-credits">
+                        <p>
+                            All Site Content TM and 2020 DC Enterteinment, unless otherwise <a href="">noted here</a>. All rights reserved. <a href="">Coockies Settings</a>
+                        </p>
+                    </div>
+                </div>
+
+                <div class="footer-logo">
+                    <img src="{{ asset('storage/assets/images/dc-logo-bg.png') }}" alt="">
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="secondary-footer">
+        <div class="container">
+            <div class="secondary-footer-wrapper">
+                <div class="secondary-footer-sign-up">
+                    <a href="">SIGN-UP NOW!</a>
+                </div>
+        
+                <div class="secondary-footer-social">
+                    <h3>
+                        FOLLOW US
+                    </h3>
+                    <ul>
+                        <li>
+                            <a href=""><img src="{{ asset('storage/assets/images/footer-facebook.png') }}" alt=""></a>
+                        </li>
+                        <li>
+                            <a href=""><img src="{{ asset('storage/assets/images/footer-twitter.png') }}" alt=""></a>
+                        </li>
+                        <li>
+                            <a href=""><img src="{{ asset('storage/assets/images/footer-youtube.png') }}" alt=""></a>
+                        </li>
+                        <li>
+                            <a href=""><img src="{{ asset('storage/assets/images/footer-pinterest.png') }}" alt=""></a>
+                        </li>
+                        <li>
+                            <a href=""><img src="{{ asset('storage/assets/images/footer-periscope.png') }}" alt=""></a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -1,15 +1,14 @@
 @extends('layouts.main-layout')
 
 @section('content')
+    {{-- JUMBOTRON --}}
     <section class="jumbotron">
         <div class="jumbotron-bg">
-            
+
         </div>
     </section>
 
-
-                    
-
+    {{-- CURRENT SERIES --}}
     <section class="current-series">
         <div class="container">
             <div class="current-series-title">
@@ -29,6 +28,46 @@
 
             <div class="load-more">
                 <button class="load-more-btn">LOAD MORE</button>            
+            </div>
+        </div>
+    </section>
+
+    {{-- INFOMRATIONS --}}
+    <section class="informations">
+        <div class="container">
+            <div class="informations-wrapper">
+                <ul>
+                    <li>
+                        <a href="">
+                            <img src="{{ asset('storage/assets/images/buy-comics-digital-comics.png') }}" alt="">
+                            <span>DIGITAL COMICS</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="">
+                            <img src="{{ asset('storage/assets/images/buy-comics-merchandise.png') }}" alt="">
+                            <span>DC MERCHANDISE</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="">
+                            <img src="{{ asset('storage/assets/images/buy-comics-subscriptions.png') }}" alt="">
+                            <span>SUBSCRIPTIONS</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="">
+                            <img src="{{ asset('storage/assets/images/buy-comics-shop-locator.png') }}" alt="">
+                            <span>COMIC SHOP LOCATOR</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="">
+                            <img src="{{ asset('storage/assets/images/buy-dc-power-visa.svg') }}" alt="">
+                            <span>DC POWER VISA</span>
+                        </a>
+                    </li>
+                </ul>
             </div>
         </div>
     </section>
