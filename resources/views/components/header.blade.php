ac54e1bc4d37f0a1453c65bd91d9e3e01139d868
Marco Orazio Re <marcooraziore@gmail.com>
2021-05-17 15:57:33 +0200

    Header section completed
    - Added primary header
    - Added secondary header
    
    Known Issues:
    - Need Refactoring 
    - Detail review

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,3 +1,61 @@
-<h1>
-    I am the header
-</h1>
+<header>
+    <div class="secondary-header">
+        <div class="container">
+            <div class="secondary-header-wrapper">
+                <ul>
+                    <li>
+                        <span class="secondary-header-voices">DC POWER VISA</span>
+                    </li>
+                    <li>
+                        <span class="secondary-header-voices">ADDITIONAL DC SITES </span><i class="fas fa-caret-down"></i>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+
+    <div class="primary-header">
+        <div class="container">
+            <div class="primary-header-wrapper">
+                <div class="logo">
+                    <img src="{{ asset('storage/assets/images/dc-logo.png') }}" alt="">
+                </div>
+
+                <ul>
+                    <li>
+                        <a href="">CHARACTERS</a>
+                    </li>
+                    <li>
+                        <a href="">COMICS</a>
+                    </li>
+                    <li>
+                        <a href="">MOVIES</a>
+                    </li>
+                    <li>
+                        <a href="">TV</a>
+                    </li>
+                    <li>
+                        <a href="">GAMES</a>
+                    </li>
+                    <li>
+                        <a href="">COLLECTIBLES</a>
+                    </li>
+                    <li>
+                        <a href="">VIDEOS</a>
+                    </li>
+                    <li>
+                        <a href="">FANS</a>
+                    </li>
+                    <li>
+                        <a href="">NEWS</a>
+                    </li>
+                    <li>
+                        <a href="">SHOP <i class="fas fa-caret-down"></i></a>
+                    </li>
+                </ul>
+
+                <input class="primary-header-search" type="text">
+            </div>
+        </div>
+    </div>
+</header>
--- resources/views/layouts/main-layout.blade.php
+++ resources/views/layouts/main-layout.blade.php
@@ -4,12 +4,21 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    {{-- FONT AWESOME --}}
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+
+    {{-- GOOGLE FONTS --}}
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet"> 
+    
     <title>Laravel Comics - Home</title>
+
+    <link rel="stylesheet" href="{{ asset('/css/app.css')}}">
 </head>
 <body>
     @include('components.header')
 
-    <img src="{{ asset('storage/assets/images/jumbotron.jpg') }}" alt="">
     @yield('content')
 
     @include('components.footer')
